9813dbf016ae0e82d8f09ba36768fa60cf547623
stu102871 <102871@glr.nl>
2026-01-21 13:58:37 +0100

    Primary Module Prep

---
+++ scripts/modules/dataConvert.js
@@ -0,0 +1,8 @@
+/**
+ * @module scripts/modules/dataConvert.js
+ * 
+ * ES6 Module Usage:
+ * - Export: Add "export" before function/class declarations
+ * - Import: import { functionName } from "../modules/dataConvert.js";
+ * - HTML: Add type="module" to <script> tag
+ */
---
+++ scripts/modules/db.js
@@ -0,0 +1,19 @@
+/**
+ * @module scripts/modules/db.js
+ * 
+ * ES6 Module Usage:
+ * - Export: Add "export" before function/class declarations
+ * - Import: import { functionName } from "../modules/db.js";
+ * - HTML: Add type="module" to <script> tag
+ */
+
+
+export function dbCollect() {}
+
+function dbDataSort() {}
+
+
+
+export function dbWrite() {}
+
+export function dbRead() {}
