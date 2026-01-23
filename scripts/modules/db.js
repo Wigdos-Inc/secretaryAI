79e7b94514e52216ecf129351fdadc4850cb0ce8
stu102871 <102871@glr.nl>
2026-01-23 14:02:43 +0100

    More Module Prep

--- scripts/modules/db.js
+++ scripts/modules/db.js
@@ -1,6 +1,8 @@
 /**
  * @module scripts/modules/db.js
  * 
+ * Module for all DB Interactions
+ * 
  * ES6 Module Usage:
  * - Export: Add "export" before function/class declarations
  * - Import: import { functionName } from "../modules/db.js";
